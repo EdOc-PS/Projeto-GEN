81353f6d8ef17068ba798c6bf9659cde7ad909b9
Eduardo Octávio <eeuardooctavio@gmail.com>
2024-02-07 23:18:03 -0300

    up5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,19 @@
 import { Routes } from '@angular/router';
 import { CardapioComponent } from './pages/cardapio/cardapio.component'
+import { AccountComponent } from './pages/client/account/account.component';
 export const routes: Routes = [
   {
     path: '',
     component: CardapioComponent
-}
+  },
+  {
+    path: 'account',
+    component: AccountComponent,
+    children:[
+      {
+        path: 'singup',
+        component: CardapioComponent,
+      }
+    ]
+  }
 ];
